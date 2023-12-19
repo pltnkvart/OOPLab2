23a89e9398866e34f99fcd14654e92a5518c9d5e
pltnkvart <artosi@yandex.ru>
2023-12-19 21:33:48 +0300

    Prettier repo

--- tests/cmake-build-debug-coverage/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/plotnikovartem/CLionProjects/OOP/Lab2/tests
-# Build directory: /Users/plotnikovartem/CLionProjects/OOP/Lab2/tests/cmake-build-debug-coverage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
